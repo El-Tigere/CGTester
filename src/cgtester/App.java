8d788ce73fa763f274c513b55ad9d821f6476b4b
El_Tigere <eltigere@web.de>
2023-12-10 14:22:13 +0100

    added MainWindow and main method

---
+++ src/cgtester/App.java
@@ -0,0 +1,9 @@
+package cgtester;
+
+public class App {
+    
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+    
+}
---
+++ src/cgtester/GLEvents.java
@@ -0,0 +1,32 @@
+package cgtester;
+
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+public class GLEvents implements GLEventListener {
+    
+    private boolean[] keyStates;
+    
+    public GLEvents(Keys keys) {
+        keyStates = keys.keyStates;
+    }
+    
+    @Override
+    public void init(GLAutoDrawable drawable) {
+    }
+    
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        System.out.println(keyStates[KeyEvent.VK_H]);
+    }
+    
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    }
+    
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+    }
+    
+}
