13998497e8a44ec596611e8e0a2725aa20afcfc2
El_Tigere <eltigere@web.de>
2023-12-21 23:30:07 +0100

    fixed camera aspect ratio

--- src/cgtester/scene/Camera.java
+++ src/cgtester/scene/Camera.java
@@ -7,18 +7,19 @@ import com.jogamp.opengl.math.Vec3f;
 public class Camera {
     
     public Vec3f position, rotation;
-    public float fov, near, far;
+    public float fov, aspect, near, far;
     
-    public Camera(Vec3f position, Vec3f rotation, float fov, float near, float far) {
+    public Camera(Vec3f position, Vec3f rotation, float fov, float aspect, float near, float far) {
         this.position = position;
         this.rotation = rotation;
         this.fov = fov;
+        this.aspect = aspect;
         this.near = near;
         this.far = far;
     }
     
     public Matrix4f getCameraMatrix() {
-        Matrix4f view = new Matrix4f().setToPerspective(fov, fov, near, far);
+        Matrix4f view = new Matrix4f().setToPerspective(fov, aspect, near, far);
         Matrix4f cameraTranslation = new Matrix4f().setToTranslation(position.mul(-1));
         Matrix4f cameraRotation = new Matrix4f().setToRotation(new Quaternion().setFromEuler(rotation).invert());
         
