8e884e707eea9247c746c6a15e498ac1601164c6
El_Tigere <eltigere@web.de>
2023-12-22 14:30:09 +0100

    fixed camera creation

--- src/cgtester/scene/Scene.java
+++ src/cgtester/scene/Scene.java
@@ -26,7 +26,7 @@ public class Scene {
     }
     
     public static Scene createTestScene() {
-        Camera mainCamera = new Camera(new Vec3f(0f, 0f, 1f), new Vec3f(0f, 0f, 0f), (float) Math.toRadians(70f), 0.1f, 100f);
+        Camera mainCamera = new Camera(new Vec3f(0f, 0f, 1f), new Vec3f(0f, 0f, 0f), (float) Math.toRadians(70f), 1f, 0.1f, 100f);
         TesterState testerState = new TesterState();
         Scene s = new Scene(mainCamera, testerState);
         return s;
