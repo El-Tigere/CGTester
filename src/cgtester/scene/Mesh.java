09408eb33edb8376aadd53ecd239ac6ef4cf4453
El_Tigere <eltigere@web.de>
2023-12-26 11:00:21 +0100

    moved mesh creation to Constructor

--- src/cgtester/scene/Mesh.java
+++ src/cgtester/scene/Mesh.java
@@ -14,37 +14,34 @@ public class Mesh {
     private int vao, vbo, ebo;
     private int indexCount;
     
-    private Mesh(GL3 gl) {
+    private Mesh(GL3 gl, float[] vertexData, int[] faceIndices) {
         this.gl = gl;
-    }
-    
-    public static Mesh fromArrays(GL3 gl, float[] vertexData, int[] faceIndices) {
-        Mesh m = new Mesh(gl);
-        m.indexCount = faceIndices.length;
+        indexCount = faceIndices.length;
+        
         IntBuffer ib;
         
         // create vao
         ib = GLBuffers.newDirectIntBuffer(1);
         gl.glGenVertexArrays(1, ib);
-        m.vao = ib.get(0);
-        gl.glBindVertexArray(m.vao);
+        vao = ib.get(0);
+        gl.glBindVertexArray(vao);
         
         // create vbo
         ib = GLBuffers.newDirectIntBuffer(1);
         gl.glGenBuffers(1, ib);
-        m.vbo = ib.get(0);
+        vbo = ib.get(0);
         // copy vertex data
         FloatBuffer vertexDataBuffer = GLBuffers.newDirectFloatBuffer(vertexData);
-        gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, m.vbo);
+        gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, vbo);
         gl.glBufferData(GL3.GL_ARRAY_BUFFER, vertexDataBuffer.limit() * 4, vertexDataBuffer, GL3.GL_STATIC_DRAW);
         
         // create ebo
         ib = GLBuffers.newDirectIntBuffer(1);
         gl.glGenBuffers(1, ib);
-        m.ebo = ib.get(0);
+        ebo = ib.get(0);
         // copy indices
         IntBuffer faceIndicesBuffer = GLBuffers.newDirectIntBuffer(faceIndices);
-        gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, m.ebo);
+        gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, ebo);
         gl.glBufferData(GL3.GL_ELEMENT_ARRAY_BUFFER, faceIndicesBuffer.limit() * 4, faceIndicesBuffer, GL3.GL_STATIC_DRAW);
         
         // vertex attributes
@@ -54,6 +51,10 @@ public class Mesh {
         gl.glEnableVertexAttribArray(1);
         gl.glVertexAttribPointer(2, 2, GL3.GL_FLOAT, false, 8 * 4, 6 * 4);
         gl.glEnableVertexAttribArray(2);
+    }
+    
+    public static Mesh fromArrays(GL3 gl, float[] vertexData, int[] faceIndices) {
+        Mesh m = new Mesh(gl, vertexData, faceIndices);
         
         return m;
     }
