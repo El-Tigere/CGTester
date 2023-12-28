7673fec0d82d6feb03609408b5e68f6fefa308c5
El_Tigere <eltigere@web.de>
2023-12-28 13:12:24 +0100

    added fromJsonFile method to Mesh class

--- src/cgtester/scene/Mesh.java
+++ src/cgtester/scene/Mesh.java
@@ -1,10 +1,13 @@
 package cgtester.scene;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jogamp.opengl.GL3;
 import com.jogamp.opengl.util.GLBuffers;
 
@@ -54,10 +57,79 @@ public class Mesh {
         gl.glEnableVertexAttribArray(2);
     }
     
-    public static Mesh fromArrays(GL3 gl, float[] vertexData, int[] faceIndices) {
-        Mesh m = new Mesh(gl, vertexData, faceIndices, null);
+    public static Mesh fromJsonFile(GL3 gl, File jsonFile) throws IOException { // TODO: less datatstructure conversions
+        // read JOSN file
+        Scanner s = new Scanner(jsonFile);
+        StringBuilder sb = new StringBuilder();
+        while(s.hasNextLine()) sb.append(s.nextLine() + '\n');
+        s.close();
         
-        return m;
+        // create MeshProperties
+        ObjectMapper om = new ObjectMapper();
+        MeshProperties properties = om.readValue(sb.toString(), MeshProperties.class);
+        
+        // load obj
+        ArrayList<float[]> positions = new ArrayList<>();
+        ArrayList<float[]> normals = new ArrayList<>();
+        ArrayList<float[]> uvs = new ArrayList<>();
+        ArrayList<Float> vertexDataList = new ArrayList<>();
+        ArrayList<Integer> faceIndicesList = new ArrayList<>();
+        int nextFaceIndex = 0;
+        
+        Scanner objScanner = new Scanner(new File(properties.meshFile));
+        while(objScanner.hasNextLine()) {
+            String line = objScanner.nextLine();
+            // System.out.println(line);
+            if(line.startsWith("v ")) positions.add(parseFloatArray(line.substring(2)));
+            if(line.startsWith("vn ")) normals.add(parseFloatArray(line.substring(3)));
+            if(line.startsWith("vt ")) uvs.add(parseFloatArray(line.substring(3)));
+            
+            if(line.startsWith("f ")) {
+                int[][] indices = parseVertexIndices(line.substring(2));
+                // for(int[] a : indices) {
+                //     for(int b : a) {
+                //         System.out.print(b + " ");
+                //     }
+                //     System.out.println();
+                // }
+                // System.out.println(normals.size());
+                for(int[] vertex : indices) {
+                    for(float f : positions.get(vertex[0] - 1)) vertexDataList.add(f);
+                    for(float f : normals.get(vertex[2] - 1)) vertexDataList.add(f);
+                    for(float f : uvs.get(vertex[1] - 1)) vertexDataList.add(f);
+                    
+                    faceIndicesList.add(nextFaceIndex++);
+                }
+            }
+        }
+        objScanner.close();
+        
+        // convert ArrayLists to Arrays
+        float[] vertexData = new float[vertexDataList.size()];
+        for(int i = 0; i < vertexDataList.size(); i++) vertexData[i] = vertexDataList.get(i);
+        int[] faceIndices = new int[faceIndicesList.size()];
+        for(int i = 0; i < faceIndicesList.size(); i++) faceIndices[i] = faceIndicesList.get(i);
+        
+        return new Mesh(gl, vertexData, faceIndices, properties);
+    }
+    
+    private static float[] parseFloatArray(String string) {
+        String[] parts = string.split(" ");
+        float[] floats = new float[parts.length];
+        for(int i = 0; i < parts.length; i++) {
+            floats[i] = Float.parseFloat(parts[i]);
+        }
+        return floats;
+    }
+    
+    private static int[][] parseVertexIndices(String string) {
+        String[] parts = string.split(" ");
+        int[][] indices = new int[parts.length][3];
+        for(int i = 0; i < parts.length; i++) {
+            String[] subParts = parts[i].split("/");
+            for(int j = 0; j < 3; j++) indices[i][j] = Integer.parseInt(subParts[j]);
+        }
+        return indices;
     }
     
     public void bindVAO() {
@@ -69,12 +141,7 @@ public class Mesh {
     }
     
     public static class MeshProperties {
-        public VertexAttribute[] vertexAttributes;
-        
-        public static class VertexAttribute {
-            public int index, size, type, offset;
-            public boolean normalized;
-        }
+        public String meshFile;
     }
     
 }
