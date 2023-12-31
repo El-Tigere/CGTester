23642fe9b5a7679ba8390c1d7743d4206480512e
El_Tigere <eltigere@web.de>
2023-12-31 12:58:06 +0100

    added draw method

--- src/cgtester/scene/Instance.java
+++ src/cgtester/scene/Instance.java
@@ -1,13 +1,23 @@
 package cgtester.scene;
 
+import com.jogamp.opengl.math.Matrix4f;
+
 public class Instance {
     
     private Mesh mesh;
+    private Material material;
     private Transform transform;
     
-    public Instance(Mesh mesh) {
+    public Instance(Mesh mesh, Material material, Transform transform) {
         this.mesh = mesh;
-        transform = new Transform();
+        this.material = material;
+        this.transform = transform;
+    }
+    
+    public void draw(Matrix4f cameraMatrix) {
+        Matrix4f matrix = new Matrix4f(cameraMatrix).mul(transform.getTransformationMatrix());
+        material.use(matrix);
+        mesh.draw();
     }
     
     public Transform getTransform() {
