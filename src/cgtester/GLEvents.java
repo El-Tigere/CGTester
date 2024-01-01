7d474c6995707a42443111af0601648a85c49de6
El_Tigere <eltigere@web.de>
2024-01-01 15:19:23 +0100

    moved resource loading and drawing to Scene class

--- src/cgtester/GLEvents.java
+++ src/cgtester/GLEvents.java
@@ -1,24 +1,16 @@
 package cgtester;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-import java.util.Scanner;
 
 import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.opengl.GL2ES3;
 import com.jogamp.opengl.GL3;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
-import com.jogamp.opengl.math.Matrix4f;
 import com.jogamp.opengl.util.GLBuffers;
 
-import cgtester.scene.Mesh;
 import cgtester.scene.Scene;
-import cgtester.scene.Texture;
 
 public class GLEvents implements GLEventListener {
     
@@ -30,27 +22,11 @@ public class GLEvents implements GLEventListener {
     private long lastNanos;
     
     private FloatBuffer clearColor = GLBuffers.newDirectFloatBuffer(4);
-    private Mesh mesh;
-    private String vertexShaderCode;
-    private String fragmentShaderCode;
-    private int shaderProgram;
-    private int matrixUniformLocation;
-    private int samplerUniformLocation;
-    
-    private Texture texure;
     
     public GLEvents(Scene scene) {
         this.scene = scene;
         testerState = scene.getTesterState();
         lastNanos = -1;
-        
-        // load shader code
-        try {
-            vertexShaderCode = loadShaderCode("./src/cgtester/shaders/test.vsh");
-            fragmentShaderCode = loadShaderCode("./src/cgtester/shaders/test.fsh");
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
     }
     
     @Override
@@ -60,35 +36,12 @@ public class GLEvents implements GLEventListener {
         // set clear color
         clearColor.put(0, 0f).put(1, 0f).put(2, 0f).put(3, 1f);
         
-        // load Mesh
-        //mesh = Mesh.fromArrays(gl, vertices, vertIndices);
+        // init scene
         try {
-            mesh = Mesh.fromJsonFile(gl, new File("src/cgtester/resources/meshes/suzanne.json"));
+            scene.init();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        // create shaders
-        int vertexShader = compileShader(gl, vertexShaderCode, GL3.GL_VERTEX_SHADER);
-        int fragmentShader = compileShader(gl, fragmentShaderCode, GL3.GL_FRAGMENT_SHADER);
-        
-        // create shader programs
-        shaderProgram = linkShaderProgram(gl, vertexShader, fragmentShader);
-        
-        // create texture
-        try {
-            texure = Texture.fromJsonFile(gl, new File("src/cgtester/resources/textures/test.json"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        // delete shaders
-        gl.glDeleteShader(vertexShader);
-        gl.glDeleteShader(fragmentShader);
-        
-        // get uniform location
-        matrixUniformLocation = gl.glGetUniformLocation(shaderProgram, "matr");
-        samplerUniformLocation = gl.glGetUniformLocation(shaderProgram, "testSampler");
         
         // further gl settings
         gl.glPolygonMode(GL3.GL_FRONT_AND_BACK, GL3.GL_FILL);
@@ -97,7 +50,7 @@ public class GLEvents implements GLEventListener {
     
     @Override
     public void display(GLAutoDrawable drawable) {
-        
+                
         // delta Time
         long currentNanos = System.nanoTime();
         float deltaTime = lastNanos == -1 ? 0f : (currentNanos - lastNanos) / 1_000_000_000f;
@@ -110,19 +63,8 @@ public class GLEvents implements GLEventListener {
         gl.glClearBufferfv(GL2ES3.GL_COLOR, 0, clearColor);
         gl.glClear(GL3.GL_DEPTH_BUFFER_BIT);
         
-        // draw triangles
-        gl.glUseProgram(shaderProgram);
-        
-        mesh.bindVAO();
-        
-        gl.glActiveTexture(GL3.GL_TEXTURE0);
-        texure.bindTexture();
-        gl.glUniform1i(samplerUniformLocation, 0);
-        
-        Matrix4f matrix = scene.getMainCamera().getCameraMatrix();
-        gl.glUniformMatrix4fv(matrixUniformLocation, 1, false, matrix.get(new float[16]), 0);
-        
-        gl.glDrawElements(GL3.GL_TRIANGLES, mesh.getElementCount(), GL3.GL_UNSIGNED_INT, 0);
+        // draw scene
+        scene.draw();
     }
     
     @Override
@@ -132,63 +74,6 @@ public class GLEvents implements GLEventListener {
     
     @Override
     public void dispose(GLAutoDrawable drawable) {
-    }
-    
-    private String loadShaderCode(String filePath) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File(filePath));
-        StringBuilder sb = new StringBuilder();
-        while(scanner.hasNextLine()) sb.append(scanner.nextLine() + '\n');
-        scanner.close();
-        return sb.toString();
-    }
-    
-    private int compileShader(GL3 gl, String shaderCode, int shaderType) {
-        // create shader
-        int shaderID = gl.glCreateShader(shaderType);
-        gl.glShaderSource(shaderID, 1, new String[] {shaderCode}, null);
-        gl.glCompileShader(shaderID);
-        
-        // get success
-        IntBuffer ib = GLBuffers.newDirectIntBuffer(1);
-        gl.glGetShaderiv(shaderID, GL3.GL_COMPILE_STATUS, ib);
-        //System.out.println(ib.get(0));
-        
-        // get error message if necessary
-        if(ib.get(0) == 0) {
-            ByteBuffer bb = ByteBuffer.wrap(new byte[512]);
-            gl.glGetShaderInfoLog(shaderID, 512, null, bb);
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < bb.limit(); i++) {
-                sb.append((char) bb.get(i));
-            }
-            System.out.println(sb.toString());
-        }
-        
-        return shaderID;
-    }
-    
-    private int linkShaderProgram(GL3 gl, int vert, int frag) {
-        // create shader program
-        int shaderProgramID = gl.glCreateProgram();
-        gl.glAttachShader(shaderProgramID, vert);
-        gl.glAttachShader(shaderProgramID, frag);
-        gl.glLinkProgram(shaderProgramID);
-        
-        // check program status
-        IntBuffer ib = GLBuffers.newDirectIntBuffer(1);
-        gl.glGetProgramiv(shaderProgramID, GL3.GL_LINK_STATUS, ib);
-        //System.out.println(ib.get(0));
-        if(ib.get(0) == 0) {
-            ByteBuffer bb = ByteBuffer.wrap(new byte[512]);
-            gl.glGetProgramInfoLog(shaderProgramID, 512, null, bb);
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < bb.limit(); i++) {
-                sb.append((char) bb.get(i));
-            }
-            System.out.println(sb.toString());
-        }
-        
-        return shaderProgramID;
     }
     
 }
--- src/cgtester/scene/Scene.java
+++ src/cgtester/scene/Scene.java
@@ -1,23 +1,31 @@
 package cgtester.scene;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import com.jogamp.opengl.math.Matrix4f;
 import com.jogamp.opengl.math.Vec3f;
 
 import cgtester.TesterState;
+import cgtester.Util;
 
 public class Scene {
     
+    private SceneProperties properties;
+    private boolean initialized;
     private ArrayList<Instance> instances;
     private Camera mainCamera;
     private TesterState testerState;
     
-    private Scene(Camera mainCamera, TesterState testerState) {
-        this.mainCamera = mainCamera;
+    private Scene(SceneProperties properties, TesterState testerState) {
+        initialized = false;
+        this.properties = properties;
         this.testerState = testerState;
     }
     
     public Camera getMainCamera() {
+        assert initialized;
         return mainCamera;
     }
     
@@ -25,11 +33,55 @@ public class Scene {
         return testerState;
     }
     
-    public static Scene createTestScene() {
-        Camera mainCamera = new Camera(new Vec3f(0f, 0f, 1f), new Vec3f(0f, 0f, 0f), (float) Math.toRadians(70f), 1f, 0.1f, 100f);
-        TesterState testerState = new TesterState();
-        Scene s = new Scene(mainCamera, testerState);
-        return s;
+    public static Scene fromJsonFile(File jsonFile) throws IOException {
+        // create SceneProperties
+        SceneProperties properties = Util.loadFileObject(jsonFile, SceneProperties.class);
+        
+        // create scene
+        Scene scene = new Scene(properties, new TesterState());
+        
+        return scene;
+    }
+    
+    public void init() throws IOException {
+        // create camera
+        SceneProperties.CameraProperties cp = properties.camera;
+        mainCamera = new Camera(new Vec3f(cp.position), new Vec3f(cp.rotation), cp.fov, 1f, cp.near, cp.far);
+        
+        // create instances
+        instances = new ArrayList<>();
+        for(SceneProperties.InstanceProperties ip : properties.instances) {
+            Mesh mesh = ResourceManager.getFromName(ip.mesh);
+            Material material = ResourceManager.getFromName(ip.material);
+            Transform transform = new Transform(new Vec3f(ip.position), new Vec3f(ip.rotation), new Vec3f(ip.scale));
+            instances.add(new Instance(mesh, material, transform));
+        }
+        
+        // scene is initialized
+        initialized = true;
+    }
+    
+    public void draw() {
+        Matrix4f cameraMatrix = mainCamera.getCameraMatrix();
+        
+        for(Instance instance : instances) {
+            instance.draw(cameraMatrix);
+        }
+    }
+    
+    private static class SceneProperties {
+        public CameraProperties camera;
+        public InstanceProperties[] instances;
+        
+        public static class CameraProperties {
+            public float[] position, rotation;
+            public float fov, near, far;
+        }
+        
+        public static class InstanceProperties {
+            public String mesh, material;
+            public float[] position, rotation, scale;
+        }
     }
     
 }
