3333b8f65b7d2de32526ba4ff5f3392406e9ca32
El_Tigere <eltigere@web.de>
2024-01-14 18:00:31 +0100

    added UI for vertexAttributes

--- src/cgtester/TesterWindow.java
+++ src/cgtester/TesterWindow.java
@@ -2,17 +2,30 @@ package cgtester;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+
+import cgtester.TesterState.VertexAttributes;
 
 public class TesterWindow extends JFrame {
     
-    Component gLPanel;
+    private TesterState testerState;
+    
+    private Component gLPanel;
+    
+    private ButtonGroup vaGroup; // vertex attribute button group
+    private JRadioButton[] vaButtons;
     
     public TesterWindow(Component glPanel) {
         this.gLPanel = glPanel;
+        
+        testerState = TesterState.get();
         
         initComponents();
         
@@ -20,6 +33,29 @@ public class TesterWindow extends JFrame {
     }
     
     private void initComponents() {
+        // vertex Attributes
+        vaGroup = new ButtonGroup();
+        vaButtons = new JRadioButton[4];
+        vaButtons[0] = new JRadioButton("UV", false);
+        addButtonListener(vaButtons[0], () -> testerState.setVertexAttributes(VertexAttributes.UV));
+        vaButtons[1] = new JRadioButton("Position, UV", false);
+        addButtonListener(vaButtons[1], () -> testerState.setVertexAttributes(VertexAttributes.POS_UV));
+        vaButtons[2] = new JRadioButton("Position, Normal, UV", true);
+        addButtonListener(vaButtons[2], () -> testerState.setVertexAttributes(VertexAttributes.POS_NORMAL_UV));
+        vaButtons[3] = new JRadioButton("Position, Normal, Color", false);
+        addButtonListener(vaButtons[3], () -> testerState.setVertexAttributes(VertexAttributes.POS_NORMAL_COLOR));
+        
+        for(JRadioButton b : vaButtons) vaGroup.add(b);
+    }
+    
+    private void addButtonListener(AbstractButton button, Runnable runnable) {
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                runnable.run();
+                gLPanel.requestFocus();
+            }
+        });
     }
     
     private void setLayout() {
@@ -28,13 +64,26 @@ public class TesterWindow extends JFrame {
         contentPane.setLayout(l);
         
         l.setHorizontalGroup(l.createSequentialGroup()
-            .addGap(20)    
-            .addComponent(gLPanel)
-            .addGap(20)
+            .addGap(10)
+            .addGroup(l.createSequentialGroup()
+                .addComponent(gLPanel)
+                .addGap(10)
+                .addGroup(l.createParallelGroup()
+                    .addComponent(vaButtons[0])
+                    .addComponent(vaButtons[1])
+                    .addComponent(vaButtons[2])
+                    .addComponent(vaButtons[3])))
+            .addGap(10)
         );
         l.setVerticalGroup(l.createSequentialGroup()
-            .addGap(20)
-            .addComponent(gLPanel)
+            .addGap(10)
+            .addGroup(l.createParallelGroup()
+                .addComponent(gLPanel)
+                .addGroup(l.createSequentialGroup()
+                    .addComponent(vaButtons[0])
+                    .addComponent(vaButtons[1])
+                    .addComponent(vaButtons[2])
+                    .addComponent(vaButtons[3])))
             .addGap(10)
         );
     }
