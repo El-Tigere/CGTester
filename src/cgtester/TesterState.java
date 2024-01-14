6857b48290a59f73f5cd7a6448a4bc979f281db9
El_Tigere <eltigere@web.de>
2024-01-14 17:53:28 +0100

    added vertexAttributes to TesterState

--- src/cgtester/TesterState.java
+++ src/cgtester/TesterState.java
@@ -7,11 +7,13 @@ public class TesterState {
     private Runnable resetWindow;
     
     public boolean[] keyStates; // TODO: perhaps move this somewhere else
+    private VertexAttributes vertexAttributes;
     
     private TesterState(Runnable resetWindow) {
         this.resetWindow = resetWindow;
         
         keyStates = new boolean[256];
+        vertexAttributes = VertexAttributes.POS_NORMAL_UV;
     }
     
     static void create(Runnable resetWindow) {
@@ -22,6 +24,22 @@ public class TesterState {
         assert instance != null;
         
         return instance;
+    }
+    
+    public VertexAttributes getVertexAttributes() {
+        return vertexAttributes;
+    }
+    
+    void setVertexAttributes(VertexAttributes vertexAttributes) {
+        this.vertexAttributes = vertexAttributes;
+        resetWindow.run();
+    }
+    
+    public enum VertexAttributes {
+        UV,
+        POS_UV,
+        POS_NORMAL_UV,
+        POS_NORMAL_COLOR
     }
     
 }
