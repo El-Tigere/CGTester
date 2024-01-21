d3d2610c1b44d75a48aa7e00db6c63443c420a71
El_Tigere <eltigere@web.de>
2024-01-21 15:53:30 +0100

    changed background clearing

--- src/cgtester/GLEvents.java
+++ src/cgtester/GLEvents.java
@@ -1,13 +1,10 @@
 package cgtester;
 
 import java.io.IOException;
-import java.nio.FloatBuffer;
 
-import com.jogamp.opengl.GL2ES3;
 import com.jogamp.opengl.GL3;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
-import com.jogamp.opengl.util.GLBuffers;
 
 import cgtester.scene.Scene;
 
@@ -18,8 +15,6 @@ public class GLEvents implements GLEventListener {
     private Scene scene;
     
     private long lastNanos;
-    
-    private FloatBuffer clearColor = GLBuffers.newDirectFloatBuffer(4);
     
     public GLEvents(Scene scene) {
         this.scene = scene;
@@ -34,9 +29,6 @@ public class GLEvents implements GLEventListener {
     public void init(GLAutoDrawable drawable) {
         gl = drawable.getGL().getGL3();
         
-        // set clear color
-        clearColor.put(0, 0f).put(1, 0f).put(2, 0f).put(3, 1f);
-        
         // init scene
         try {
             scene.init();
@@ -46,6 +38,7 @@ public class GLEvents implements GLEventListener {
         
         // further gl settings
         gl.glPolygonMode(GL3.GL_FRONT_AND_BACK, GL3.GL_FILL);
+        gl.glClearColor(0.5f, 0f, 0.5f, 1f);
         gl.glEnable(GL3.GL_DEPTH_TEST);
     }
     
@@ -61,8 +54,7 @@ public class GLEvents implements GLEventListener {
         scene.update(deltaTime);
         
         // clear color buffer
-        gl.glClearBufferfv(GL2ES3.GL_COLOR, 0, clearColor);
-        gl.glClear(GL3.GL_DEPTH_BUFFER_BIT);
+        gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
         
         // draw scene
         scene.draw();
